63590880c6065c23d1baa26eb3bf1fd9cfd72140
Jofersun Hutagalung <jofersun@hotmail.com>
2023-07-14 18:48:11 +0700

    Add coverage report
    Add test coverage reporter
    add jest-html-reporter

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,32 @@ module.exports = {
     "^.+\\.css$": "jest-transform-css",
   },
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  coverageReporters: ["json", "lcov", "text"],
+  //json file name: coverage-final.json
+  //html file name: index.html
+  //lcov file name: lcov-report/index.html
+  //text: show on console
+  coverageDirectory: "coverage",
+  reporters: [
+    "default",
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        //configuration can be saved on file ./jesthtmlreporter.config.josn
+        pageTitle: "Unit Test Report for Movie Catalogue",
+        outputPath: "coverage/jest-html-reporter/index.html",
+        includeFailureMsg: true,
+        openReport: true,
+        darkTheme: true,
+        // urlForTestFiles: "/src/utils/index.test.js",
+        // customInfos: [
+        //   { title: "test1", value: "test1" },
+        //   { title: "test2", value: "test2" },
+        // ],
+        enableMergeData: true,
+      },
+    ],
+  ],
+
+  // testResultsProcessor: "./node_modules/jest-html-reporter",
 };
